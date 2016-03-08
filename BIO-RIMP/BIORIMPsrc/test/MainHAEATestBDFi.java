55fb1d4129a39685196e8b142d82834a99753931
danielrc <danielrcardenas@gmail.com>
2016-03-08 16:53:49 +0100

    Filtering recalling depending on refactor types

--- BIO-RIMP/BIORIMPsrc/test/MainHAEATestBDFi.java
+++ BIO-RIMP/BIORIMPsrc/test/MainHAEATestBDFi.java
@@ -81,7 +81,7 @@ public class MainHAEATestBDFi {
 		long start = System.currentTimeMillis();
 		
 		//First Step: Calculate Actual Metrics
-		String userPath = System.getProperty("user.dir");
+		String userPath = System.getProperty("user.dir")+"/BIO-RIMP";
 		//String[] args = { "-l", "Java", "-p", userPath+"\\test_data\\code\\acra\\src","-s", "     acra      " };
 		String[] args = { "-l", "Java", "-p", userPath + "/test_data/code/ccodec/src", "-s", "     ccodec      " };
 	
